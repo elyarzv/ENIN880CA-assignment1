8de03cb355b7d8a75e84bf9f1e813cfdeb785a13
Nastaran Ayazlou <nastaranayazlou@iMac.hitronhub.home>
2021-11-11 11:29:11 -0800

    comments modified

--- convolution.py
+++ convolution.py
@@ -2,25 +2,25 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Load the image
+# Load the image #
 image = cv2.imread('photos/face.jpeg', 0)
 
-## Convolution
+## Convolution #
 
-# creating kernel matrix
+# creating kernel matrix #
 kernel = np.ones((21, 21), np.float32) / (21**2)
 
-# convoluting the image withh kernel
+# convoluting the image withh kernel #
 img_uniform = cv2.filter2D(image, -1, kernel)
 
-#applying Gaussian blur on image
+#applying Gaussian blur on image #
 img_gaussian = cv2.GaussianBlur(image, (21,21), 0)
 
-# saving image in photos folder
+# saving image in photos folder #
 cv2.imwrite('photos/uniform.jpeg', img_uniform)
 cv2.imwrite('photos/guassian.jpeg', img_gaussian)
 
-# show the image 
+# show the image #
 f2 = plt.figure("convolution")
 
 f2.add_subplot(2,2,1)
